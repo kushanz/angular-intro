122aa39c172bdb017d534e22705f9234c49c0706
kushanz <kushanshanuka@yahoo.com>
2019-06-10 13:14:51 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,18 @@ import { HeaderComponent } from './template/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
 import { UsersService } from './Services/users.service';
+import { PostComponent } from './pages/post/post.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'user', component: UserComponent },
+  { path: 'post', component: PostComponent },
 ];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes)],
-  declarations: [ AppComponent, HelloComponent, MyDirectiveDirective, HeaderComponent, HomeComponent, UserComponent ],
+  declarations: [ AppComponent, HelloComponent, MyDirectiveDirective, HeaderComponent, HomeComponent, UserComponent, PostComponent ],
   bootstrap:    [ AppComponent ],
   providers: [UsersService]
 })
