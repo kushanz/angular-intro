9db6c6537cc53801dc21f25ecd2f3fa29073ea9f
kushanz <kushanshanuka@yahoo.com>
2019-06-10 15:14:12 +0530

    1 file modified

--- src/app/Services/post.service.ts
+++ src/app/Services/post.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Injectable()
 export class PostService {
 
-  constructor() { }
+  constructor(private postClient: HttpClient) { }
+
+getPost(){
+  this.postClient.get('http://tryndev.com/wp-json/wp/v2/posts')
+  .subscribe(data => {
+    console.log(data)
+  })
+}
+  
 
 }
