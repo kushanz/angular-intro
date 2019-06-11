d3b1487153ce3f2f949ee9ecd33a2dd5df13d01e
kushanz <kushanshanuka@yahoo.com>
2019-06-11 10:20:35 +0530

    2 files modified

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersService} from '../../Services/users.service'
+import {trigger,transition,style,animate} from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [UsersService]
-  
+  providers: [UsersService],
+  animations:[
+    trigger('fade',[
+      transition('void => *',[
+        style({backgroundColor:'yellow',opacity:0}),
+        animate(2000,style({backgroundColor:'white',opacity:1}))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
  name = 'Global Dev rtergt edg f';
